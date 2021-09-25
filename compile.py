0e8c08844cd01ff87d7ccdb807e5f680fdd7fc64
= <mcaden966@gmail.com>
2021-09-25 18:37:11 -0500

    updated build script for clarity

--- compile.py
+++ compile.py
@@ -1,9 +1,9 @@
 import os
 
 cc = "clang"
-cflags = " -std=c17 -Wall -Wextra -Ofast"
-cfiles = " main.c board.c"
-command = cc + cflags + cfiles + " -o GhostyChess"
+cflags = "-std=c17 -Wall -Wextra -Ofast"
+cfiles = "main.c board.c"
+command = cc + " " + cflags + " " + cfiles + " -o GhostyChess"
 
 os.system(command)
 
