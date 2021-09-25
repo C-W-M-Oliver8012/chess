7d4a12f6201978e2ae325a8b8c93a67e99a2554e
= <mcaden966@gmail.com>
2021-09-25 18:46:01 -0500

    build script makes even more sense

--- compile.py
+++ compile.py
@@ -3,7 +3,7 @@ import os
 cc = "clang"
 cflags = "-std=c17 -Wall -Wextra -Ofast"
 cfiles = "main.c board.c"
-command = cc + " " + cflags + " " + cfiles + " -o GhostyChess"
+command = "%s %s %s -o GhostyChess" % (cc, cflags, cfiles)
 
 os.system(command)
 
