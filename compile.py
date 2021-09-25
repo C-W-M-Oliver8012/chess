4cc43d9f53c82d7d755f23984bd33e6935af5986
= <mcaden966@gmail.com>
2021-09-25 18:29:26 -0500

    now use python build script

---
+++ compile.py
@@ -0,0 +1,10 @@
+import os
+
+cc = "clang"
+cflags = " -std=c17 -Wall -Wextra -Ofast"
+cfiles = " main.c board.c"
+command = cc + cflags + cfiles + " -o GhostyChess"
+
+os.system(command)
+
+print(command)
