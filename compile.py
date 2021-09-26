fd98bccd1a18315540bf5fdc86149c9e88b3c0da
= <mcaden966@gmail.com>
2021-09-25 22:25:50 -0500

    added march=native to build script

--- compile.py
+++ compile.py
@@ -1,7 +1,7 @@
 import os
 
 cc = "clang"
-cflags = "-std=c18 -Wall -Wextra -Ofast"
+cflags = "-std=c18 -Wall -Wextra -Ofast -march=native"
 cfiles = "main.c board.c"
 command = "%s %s %s -o GhostyChess" % (cc, cflags, cfiles)
 
