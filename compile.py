356e3b5ba34fb27b469144f7c76c64b4c28dfcf5
= <mcaden966@gmail.com>
2021-09-25 19:03:52 -0500

    changed the standard from c17 to c18 as that is more correct

--- compile.py
+++ compile.py
@@ -1,7 +1,7 @@
 import os
 
 cc = "clang"
-cflags = "-std=c17 -Wall -Wextra -Ofast"
+cflags = "-std=c18 -Wall -Wextra -Ofast"
 cfiles = "main.c board.c"
 command = "%s %s %s -o GhostyChess" % (cc, cflags, cfiles)
 
